2a3605063ef1677eaa154b2d0dbbc8622c18ab9c
Sreekar Reddy Potham <sreekarreddytope@gmail.com>
2024-01-13 07:58:21 +0530

    Double LL

---
+++ doublelinked.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+    struct node *prev;
+} *head;
+
+void create(){
+    struct node *newnode, *temp;
+    int choice = 1;
+    head = 0;
+    while(choice){
+        newnode = (struct node *)malloc(sizeof(struct node));
+        printf("Enter data");
+        scanf("%d", &newnode->data);
+        newnode ->prev = 0;
+        newnode->next = 0;
+        if (head==0){
+            head = temp = newnode;
+        }
+        else{
+            temp->next = newnode;
+            newnode->prev = temp;
+            temp = newnode;
+        }
+        printf("Do you want to continue(0,1): ");
+        scanf("%d", &choice);
+    }
+}
+void display(){
+    struct node *temp;
+    temp = head;
+    while(temp!=0){
+        printf("%d ",temp->data);
+        temp= temp->next;
+    }
+}
+
+void main(){
+    create();
+    display();
+}
